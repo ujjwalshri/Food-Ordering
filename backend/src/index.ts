5dcb156ebdba14e4ae46b1a3a687cb78ebd40917
ujjwalshri <128953832+ujjwalshri@users.noreply.github.com>
2024-10-19 00:38:58 +0530

    Connected to mongoDB atlas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";    
+import 'dotenv/config';
+
+
 
 const app = express();
 
@@ -9,6 +13,10 @@ app.use(cors());
 app.get('/test', (req, res) => {
     res.json({mssg: 'Hello World'});
 });
+
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING as string).then(()=> console.log('Connected to MongoDB')).catch((err) => console.log(err));
+
 
 app.listen(4000, () => {
     console.log(`Server is running on port ${4000}`);
